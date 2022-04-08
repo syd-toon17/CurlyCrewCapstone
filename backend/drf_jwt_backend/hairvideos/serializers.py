1f58c4bd77b9388fb5865e959623a0c5f0aaa5ca
Sydney Toon <sydneytoon@Sydneys-MacBook-Pro.local>
2022-04-08 13:37:35 -0500

    Completed serializers + urls for hair video model.

---
+++ backend/drf_jwt_backend/hairvideos/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Comment, Reply
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id','user', 'video_id', 'text', 'likes', 'dislikes']
+        depth = 1
+        
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields =['id', 'user','comment_id', 'text']
+        depth = 1
+
+    comment_id = serializers.IntegerField(write_only = True)
---
+++ backend/drf_jwt_backend/hairvideos/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from hairvideos import views
+
+urlpatterns = [
+    path('comment/<str:video_id>/', views.get_by_video_id),
+    path('add_comment/<str:video_id>', views.add_comment),
+    path('new_comment/<int:comment_id>', views.user_comments),
+    path('new_reply/<int:comment_id>/', views.user_replies),
+    path('edit_comment/<int:comment_id>/', views.user_comments),
+    path('comment_replies/<int:comment_id>/', views.comment_replies)
+]
